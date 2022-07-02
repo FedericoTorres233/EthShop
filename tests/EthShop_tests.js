9fe668454931e185bf48a2aaf280a0d43309acbc
FedericoTorres233 <federicofederico881@gmail.com>
2022-07-02 20:08:31 -0300

    feat: add second test

--- tests/EthShop_tests.js
+++ tests/EthShop_tests.js
@@ -20,11 +20,27 @@ contract("Ethshop", (account) => {
 
   // This code executes after the contract is created
   context("Execution", async () => {
+    // First test
     it("Should deposit to contract", async () => {
       const balance_sent = Math.trunc(Math.random() * 1000000000).toString();
       await EthshopInstance.send(balance_sent);
       const balance = await web3.eth.getBalance(EthshopInstance.address);
       expect(balance).to.equal(balance_sent);
+    });
+
+    // Second test
+    it("Should withdraw all funds from contract", async () => {
+      await EthshopInstance.send(
+        Math.trunc(Math.random() * 1000000000).toString()
+      );
+      const balance_before = await web3.eth.getBalance(EthshopInstance.address);
+      await EthshopInstance.withdrawFunds(
+        await web3.eth.getBalance(EthshopInstance.address),
+        { from: owner }
+        );
+      const balance_after = await web3.eth.getBalance(EthshopInstance.address);
+      expect(+balance_after).to.equal(0);
+      expect(+balance_before).to.be.above(0);
     });
   });
 });
